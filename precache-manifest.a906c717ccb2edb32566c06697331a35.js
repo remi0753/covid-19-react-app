edbf3652fbe83565c38d58da99c902fe56b5d554
remi <sawdinocrisis@gmail.com>
2020-05-06 18:08:12 +0900

    Updates

---
+++ precache-manifest.a906c717ccb2edb32566c06697331a35.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8fcb03ce7ea5e29247e8dacc9dd0ad0",
+    "url": "/covid-19-react-app/index.html"
+  },
+  {
+    "revision": "7b4b85371d2119ea4c94",
+    "url": "/covid-19-react-app/static/css/main.ee87346d.chunk.css"
+  },
+  {
+    "revision": "d311f18a282445cb46d9",
+    "url": "/covid-19-react-app/static/js/2.5f99b4e2.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/covid-19-react-app/static/js/2.5f99b4e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7b4b85371d2119ea4c94",
+    "url": "/covid-19-react-app/static/js/main.7d4e8d62.chunk.js"
+  },
+  {
+    "revision": "5675f31525c9050a21cb",
+    "url": "/covid-19-react-app/static/js/runtime-main.08f7bebf.js"
+  }
+]);
